ddfbb31a26afbbba82b1b6bd26339385280c80e2
Rebound <eliazbp92@gmail.com>
2026-08-18 15:25:16 -0700

    Update Nightmare Rush.lua

--- Nightmare Rush.lua
+++ Nightmare Rush.lua
@@ -1,26 +1,33 @@
--- Define the globals
-local workspaceService = game:GetService("Workspace")
-local running = true -- Set to true by default so the loop starts running
+game.Players.LocalPlayer.PlayerGui.MainUI.Jumpscare.Jumpscare_Rush.ImageLabel.ImageColor3 = Color3.fromRGB(255, 0, 0)
+game.Players.LocalPlayer.PlayerGui.MainUI.Jumpscare.Jumpscare_Rush.ImageLabelBig.ImageColor3 = Color3.fromRGB(255,0,0)
+
+local running = true
 
 local function OnRushSpawned(model)
     print("Rush detected:", model.Name)
     task.wait(0.5)
-    if game.Workspace:FindFirstChild("RushMoving") then
-        game.Workspace.RushMoving.RushNew.Attachment.BlackTrail.Color = ColorSequence.new({
-            ColorSequenceKeypoint.new(0, Color3.new(0.407843, 0, 0)),
-            ColorSequenceKeypoint.new(0, Color3.new(0.407843, 0, 0)),
-            ColorSequenceKeypoint.new(0.407843, Color3.new(0.407843, 0, 0)),
-            ColorSequenceKeypoint.new(0.407843, Color3.new(0.407843, 0, 0)),
-            ColorSequenceKeypoint.new(1, Color3.new(0.407843, 0, 0))
-        })
-        game.Workspace.RushMoving.RushNew.Attachment.ParticleEmitter.Color = ColorSequence.new({
-            ColorSequenceKeypoint.new(0, Color3.new(1, 0, 0)),
-            ColorSequenceKeypoint.new(0, Color3.new(1, 0, 0)),
-            ColorSequenceKeypoint.new(1, Color3.new(1, 0, 0))
-        })
-		--
-		game.Workspace.RushMoving.RushNew.PlaySound.PlaybackSpeed = 0.05
-		game.Workspace.RushMoving.RushNew.PlaySound.Footsteps = 0.05
+    local rushNew = workspaceService:WaitForChild("RushMoving"):WaitForChild("RushNew")
+    rushNew.Attachment.ParticleEmitter.Color = ColorSequence.new(Color3.fromRGB(255,0,0))
+    rushNew.PlaySound.PlaybackSpeed = 0.1
+    rushNew.Footsteps.PlaybackSpeed = 0.1
+    workspaceService.RushMoving.RushNew.Attachment.BlackTrail.Color = ColorSequence.new(Color3.fromRGB(41,1,1))
+end
+
+task.spawn(function()
+    while task.wait(0.1) do
+        if running then
+            local rush = workspaceService:FindFirstChild("RushMoving")
+            if rush and rush:IsA("Model") then
+                running = false
+                task.spawn(function()
+                    OnRushSpawned(rush)
+                end)
+                repeat
+                    task.wait(0.1)
+                    rush = workspaceService:FindFirstChild("RushMoving")
+                until not rush
+                running = true
+                print("Rush gone, loop resumed")
             end
         end
     end
